64d0ff81a2a8cb729ad63e5247f447f87545aba1
MetheuAA <matheo200817@gmail.com>
2024-05-15 11:22:38 -0400

    added Bullet class, draw method and collideWith method

---
+++ src/javascript/Bullet.js
@@ -0,0 +1,32 @@
+class Bullet {
+    constructor(canvas, x, y, velocity,  BulletColor){
+        this.canvas = canvas;
+        this.x = x;
+        this.y = y;
+        this.velocity = velocity;
+        this.BulletColor = BulletColor;
+
+        this.width = 5
+        this.height = 20
+
+    }
+
+draw(ctx) { 
+    this.y -= this.velocity
+    ctx.fillStyle = this.BulletColor
+    ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+    collide(sprite) {
+        if(
+       this.x + this.width > sprite.x &&
+        this.x < sprite.x + sprite.width &&
+        this.y + this.height > sprite.y &&
+        this.y < sprite.y + sprite.height
+        ) {
+            return true;
+        } else { 
+            return false
+        }
+    }
+}
