18c266593f48d4af12a561f79e8ec7f2cba3fa47
MetheuAA <matheo200817@gmail.com>
2024-05-22 10:18:14 -0400

    add fireBullet function

--- src/javascript/EnemyController.js
+++ src/javascript/EnemyController.js
@@ -40,5 +40,14 @@ constructor(canvas, enemyBulletController, playerBulletController) {
         });
         this.enemyRows = this.enemyRows.filter((enemyRow) => enemyRow.lenght > 0);
     }
+    fireBullet(){
+        this.fireBulletTimer--;
+        if(this.fireBulletTimer <= 0) {
+            const allEnemies = this.enemyRows.flat();
+            const enemyIndex = Math.floor(Math.random() * allEnemies.lenght);
+            const enemy = allEnemies[enemyIndex];
+            this.enemyBulletController.shoot(enemy.x + enemy.width / 2, enemy.y, -3);
+        }
+    }
 }
 
