84ba370e3ff2c660772db5340185a35d949a48f6
MetheuAA <matheo200817@gmail.com>
2024-05-27 11:12:49 -0400

    fix

--- src/javascript/Enemy.js
+++ src/javascript/Enemy.js
@@ -1,30 +1,28 @@
-class Enemy {
+export default class Enemy {
     constructor(x, y, imageNumber) {
-        this.x = x;
-        this.y = y;
-        this.imageNumber = imageNumber;
-        this.width = 44;
-        this.height = 32;
-        this.image = new Image();
-        this.image.src = `src/assets/images/enemy${this.imageNumber}.png`;
+    this.x = x;
+    this.y = y;
+    this.width = 44;
+    this.height = 32;
+    thisimage = new Image();
+    thisimage.src = `src/assets/images/enemy${imageNumber}.png`;
     }
+    
     draw(ctx) {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
     move(xVelocity, yVelocity) {
-        this.x += xVelocity;
-        this.y += yVelocity;
+    this.x += xVelocity;
+    this.y += yVelocity;
     }
-    collide(sprite) {
-        if (
-            this.x + this.width > sprite.x &&
-            this.x < sprite.x + sprite.width &&
-            this.y + this.height > sprite.y &&
-            this.y < sprite.y + sprite.height
-        ) {
-            return true;
-        } else {
-            return false;
-        }
+    collideWith(sprite) {
+    if (this.x < sprite.x + sprite.width &&
+    this.x + this.width > sprite.x &&
+    this.y < sprite.y + sprite.height &&
+    this.height + this.y > sprite.y) {
+    return true;
+    } else {
+    return false;
     }
-}
+    }
+    }
